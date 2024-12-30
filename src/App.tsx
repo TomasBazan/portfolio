15ad3a1e1bb06334d9e212eae589d83a13df6b15
Tomas <tomasbazan5@gmail.com>
2024-12-30 15:26:28 -0300

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home"
+import PageWrapper from "./layouts/PageWrapper"
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <PageWrapper>
+        <Home />
+      </PageWrapper>
+    </div>)
+}
+
+export default App
