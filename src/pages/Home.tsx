466e47e5d3806310a6077c1502082adf8c661fa0
Tomas <tomasbazan5@gmail.com>
2025-01-21 19:08:39 -0300

    feat: photo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 export function Home() {
   return (
-    <div id="About" className="flex justify-center bg-[var(--background-color-secondary)]">
-      <section className="min-h-[calc(100vh-3rem)] justify-center justify-items-center content-center max-w-full 2xl:max-w-5xl xl:max-w-5xl lg:max-w-3xl md:max-w-xl">
-        <h1 className="text-3xl md:text-5xl">Tomas Bazan</h1>
-        <p>
-          I am a computer science student at FAMAF university with a passion for web development. I have
-          experience with JavaScript, TypeScript, React, Python and Django. I am
-          currently looking for new opportunities to grow as a developer.
-        </p>
+    <div id="About" className="">
+      <section className="min-h-[calc(100vh-3rem)] flex flex-col justify-center items-center w-full text-center space-y-6">
+          <h1 className="text-3xl md:text-5xl xl:text-8xl">Tomas Bazan</h1>
+          <p className="pt-9 text-lg md:text-xl xl:text-2xl max-w-3xl">
+            I am a computer science student at FAMAF university with a passion for web development. I have
+            experience with JavaScript, TypeScript, React, Python and Django. I am
+            currently looking for new opportunities to grow as a developer.
+          </p>
       </section>
     </div>
   );
