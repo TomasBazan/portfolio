4c5ad98b927ef3cde83ae8076e1fbbef44a76d54
Tomas <tomasbazan5@gmail.com>
2025-01-21 10:26:39 -0300

    fix: add scroll support for glowing effect

--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -9,21 +9,40 @@ interface projectInfo{
 }
 
 
-export function CardProject( {repoPath, name, desc, gridLayout} : projectInfo)  {
+export function CardProject( {repoPath, name, description, gridLayout} : projectInfo)  {
   const cardRef = useRef<HTMLDivElement>(null);
+  const mousePosition = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
 
-  const applyOverlayMask = (e: MouseEvent) => {
+  const applyOverlayMask = () => {
     if (!cardRef.current) return;
-    const x = e.pageX - cardRef.current.offsetLeft;
-    const y = e.pageY - cardRef.current.offsetTop;
+
+    const rect = cardRef.current.getBoundingClientRect();
+    const x = mousePosition.current.x - rect.left;
+    const y = mousePosition.current.y - rect.top;
     cardRef.current.style.setProperty('--mouse-x', `${x}px`);
     cardRef.current.style.setProperty('--mouse-y', `${y}px`);
   };
 
+  const handleMouseMove = (e: MouseEvent) => {
+    mousePosition.current = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+    applyOverlayMask();
+  };
+
+  const handleScroll = () => {
+    applyOverlayMask();
+  };
+
   useEffect(() => {
-    document.body.addEventListener("pointermove", (e) => {
-      applyOverlayMask(e);
-    });
+    document.body.addEventListener('pointermove', handleMouseMove);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.body.removeEventListener('pointermove', handleMouseMove);
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [])
  
   return (
@@ -33,7 +52,7 @@ export function CardProject( {repoPath, name, desc, gridLayout} : projectInfo)  
     >
       <article className="card-content min-h-80 bg-[#111111] rounded-lg w-full h-full flex flex-col items-center justify-between text-pretty">
         <h2 className="text-4xl">{`${name}`}</h2>
-        <p className="text-center text-white text-xl max-w-[300px] mx-auto p-2">{`${desc}`}</p>
+        <p className="text-center text-white text-xl max-w-[300px] mx-auto p-2">{`${description}`}</p>
         <button>
           <a href={repoPath ? `https://github.com/TomasBazan/${repoPath}` : `https://github.com/TomasBazan?tab=repositories`} target="_blank" rel="noopener noreferrer" className="text-cyan-300">
             GitHub
